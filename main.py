fbab437234d68adf90ab74d316822439dc023e3b
mazen <xpmazen18@gmail.com>
2024-11-24 02:24:53 +0200

    import-crop

--- main.py
+++ main.py
@@ -3,29 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-def crop_barcode(img):
-    """
-    Crops the barcode region from the image using contours.
-    """
-    # Find contours in the binary image
-    contours, _ = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-
-    if not contours:
-        print("No contours detected! Skipping this image.")
-        return img  # Return the original image if no contours are found
-
-    # Find the horizontal limits of the barcode
-    x_min = min([cv.boundingRect(contour)[0] for contour in contours])  # Leftmost x-coordinate
-    x_max = max([cv.boundingRect(contour)[0] + cv.boundingRect(contour)[2] for contour in contours])  # Rightmost x-coordinate
-
-    # Assume the largest contour corresponds to the vertical extent of the barcode
-    largest_contour = max(contours, key=cv.contourArea)
-    _, y, _, h = cv.boundingRect(largest_contour)  # Get the y and height (vertical cropping)
-
-    # Crop the image so that only the barcode is visible
-    cropped_img = img[y:y + h, x_min:x_max]
-
-    return cropped_img
+from crop import crop_barcode
 
 def display_image(img, title):
     """
@@ -99,5 +77,6 @@ def process_test_cases(image_folder):
 
 if __name__ == "__main__":
     # Replace with the path to your test cases folder
-    image_folder = "C:\Users\A\Desktop\CV-main\Test Cases-20241123"
+    image_folder = "C:\\Users\\A\\Desktop\\CV-main\\Test Cases-20241123"
+
     process_test_cases(image_folder)
